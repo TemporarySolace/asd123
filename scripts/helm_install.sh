04a0bf302a10326fcb95826bf74dd9325df6ff1f
Nabeel Shuaib <nabeelshuaib1@gmail.com>
2025-06-17 15:24:23 -0500

    restructure

--- scripts/helm_install.sh
+++ scripts/helm_install.sh
@@ -11,6 +11,12 @@ helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
+
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/prometheus.yaml
+
 echo "[INFO] Installing Istio components..."
 helm install istio-base istio/base -n istio-system --create-namespace
 helm install istiod istio/istiod -n istio-system
@@ -19,3 +25,8 @@ helm install ingressgateway istio/gateway -n istio-system
 echo "[INFO] Installing RabbitMQ..."
 helm install rabbitmq bitnami/rabbitmq -n rabbitmq --create-namespace -f ../charts/rabbitmq/values-pv.yaml
 kubectl label namespace rabbitmq istio-injection=enabled --overwrite
+
+helm repo add kiali https://kiali.org/helm-charts
+helm repo update
+
+helm install --namespace istio-system kiali-server kiali/kiali-server --create-namespace
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -20,35 +20,10 @@ source ./wait_for_pods.sh istio-system
 echo "[INFO] System should now be accessible via Istio ingress."
 
 
-# === Docker image build ===
-echo "[INFO] Building Docker images..."
-docker build -t producer:latest ../docker/producer || { echo '[ERROR] Failed to build producer image.'; exit 1; }
-docker build -t consumer:latest ../docker/consumer || { echo '[ERROR] Failed to build consumer image.'; exit 1; }
-
-# === Load images into Kind ===
-echo "[INFO] Loading images into Kind cluster..."
-kind load docker-image producer:latest --name test || { echo '[ERROR] Failed to load producer image into Kind.'; exit 1; }
-kind load docker-image consumer:latest --name test || { echo '[ERROR] Failed to load consumer image into Kind.'; exit 1; }
-
-# Apply messaging manifests
-kubectl apply -f ../manifests/messaging/producer-deployment.yaml
-kubectl apply -f ../manifests/messaging/producer-service.yaml
-kubectl apply -f ../manifests/messaging/consumer-deployment.yaml
-kubectl apply -f ../manifests/messaging/consumer-service.yaml
-
-# Apply messaging ingress configs
-kubectl apply -f ../manifests/messaging/producer-gateway.yaml
-kubectl apply -f ../manifests/messaging/producer-virtualservice.yaml
-kubectl apply -f ../manifests/messaging/consumer-gateway.yaml
-kubectl apply -f ../manifests/messaging/consumer-virtualservice.yaml
-kubectl apply -f ../charts/istio/templates/gateway.yaml
-kubectl apply -f ../charts/istio/templates/virtualservices.yaml
 # Apply RabbitMQ manifests
-kubectl apply -f ../charts/rabbitmq/templates/deployment.yaml
-kubectl apply -f ../charts/rabbitmq/templates/service.yaml
-kubectl apply -f ../manifests/rabbitmq/rabbitmq-gateway.yaml
-kubectl apply -f ../manifests/rabbitmq/rabbitmq-virtualservice.yaml
-kubectl apply -f ../manifests/istio/istio-gateway.yaml
+kubectl apply -f ../manifests/rabbitmq/
+kubectl apply -f ../manifests/istio/
+kubectl apply -f ../manifests/telemetry/prometheus-virtualservice.yaml -n monitoring
 
 #show all pods
 echo "[INFO] Displaying all pods in the cluster..."
